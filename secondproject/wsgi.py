303d00beb36debed57b45b01c3a7b1bf4808d67a
dohyungLee <hydrogen1104@gmail.com>
2019-07-31 18:28:03 +0900

    heroku

--- secondproject/wsgi.py
+++ secondproject/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE',"secondproject")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE','secondproject.setting')
 
 application = get_wsgi_application()
